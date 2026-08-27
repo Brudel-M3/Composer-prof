d122e83a3c7395af95a0591dc3aa9d1c42e81c97
Brudel-M3 <brudelboukaka36@gmail.com>
2026-08-27 12:10:16 +0100

    Initialize App.jsx with project configuration

---
+++ scr/App.jsx
@@ -0,0 +1,20 @@
+{
+  "name": "ArsHarmonia",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "start": "react-native start",
+    "android": "react-native run-android"
+  },
+  "dependencies": {
+    "react": "18.2.0",
+    "react-native": "0.74.5"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.24.0",
+    "@babel/preset-env": "^7.24.0",
+    "@babel/runtime": "^7.24.0",
+    "@react-native/babel-preset": "0.74.5",
+    "@react-native/metro-config": "0.74.5"
+  }
+}
